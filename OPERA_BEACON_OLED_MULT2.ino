9eb6a348aa5a0bd7a0d8b5609b74ff2d74e31706
EA5JTT <joanpao@gmail.com>
2025-12-18 16:42:55 +0100

    Update OPERA_BEACON_OLED_MULT2.ino

--- OPERA_BEACON_OLED_MULT2.ino
+++ OPERA_BEACON_OLED_MULT2.ino
@@ -35,6 +35,22 @@ OperaBand bands[] = {
   {1406221000ULL,"20m",  "OPERA1",  256,  1UL  * 60UL * 1000UL, 1UL * 60UL * 1000UL}
 };
 */
+/*
+ * Copyright (C) 2025 Juan Pablo Sanchez EA5JTT
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
 
 #include <si5351.h>
 #include <SPI.h>
